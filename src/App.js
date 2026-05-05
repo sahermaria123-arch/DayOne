c5c66d2f9c8ee05a037768d5caac6433a82a9394
saher maria <sahermaria123@gmail.com>
2026-05-04 19:30:20 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import DayOneGit from './Components/DayOneGit';
+
+function App() {
+  return (
+    <div className="App">
+      <DayOneGit/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/DayOneGit.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DayOneGit = () => {
+  return (
+    <div>
+      <h1> Day one   </h1>
+    </div>
+  )
+}
+
+export default DayOneGit
