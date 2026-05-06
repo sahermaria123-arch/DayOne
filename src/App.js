c2d937524fe08e69b4fb1f0f670d7087ef6e2147
saher maria <sahermaria123@gmail.com>
2026-05-06 09:25:03 -0700

    create day two file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import DayOneGit from './Components/DayOneGit';
+import Daytwo from './Components/Daytwo';
 
 function App() {
   return (
     <div className="App">
       <DayOneGit/>
+      <Daytwo/>
     </div>
   );
 }
---
+++ src/Components/Daytwo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Daytwo = () => {
+  return (
+    <div>
+      <h1> Day Two On Git. </h1>
+    </div>
+  )
+}
+
+export default Daytwo;
