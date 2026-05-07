1ef30283bc92e6da2df32edd02828942331f97f1
saher maria <sahermaria123@gmail.com>
2026-05-06 21:15:06 -0700

    add file on friday

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import DayOneGit from './Components/DayOneGit';
 import Daytwo from './Components/Daytwo';
+import Friday from './Components/Friday';
 
 function App() {
   return (
     <div className="App">
       <DayOneGit/>
       <Daytwo/>
+      <Friday/>
     </div>
   );
 }
---
+++ src/Components/Friday.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Friday = () => {
+  return (
+    <div>
+       <h1> Friday Work. </h1>
+    </div>
+  )
+}
+
+export default Friday;
