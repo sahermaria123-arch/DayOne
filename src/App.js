c3c0841b0360a15e36ae6c09681f0abde02b4bb4
Fahad Bhatti <fahadbhatti4win@gmail.com>
2026-05-07 21:13:12 +0500

    Create This file Hello Work

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import DayOneGit from './Components/DayOneGit';
 import Daytwo from './Components/Daytwo';
 import Friday from './Components/Friday';
+import HelloWork from './Components/HelloWork';
 
 function App() {
   return (
     <div className="App">
-      <DayOneGit/>
+      {/* <DayOneGit/>
       <Daytwo/>
-      <Friday/>
+      <Friday/> */}
+      <HelloWork/>
     </div>
   );
 }
---
+++ src/Components/HelloWork.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HelloWork = () => {
+  return (
+    <div>
+      <h1>Hello Work</h1>
+    </div>
+  )
+}
+
+export default HelloWork
